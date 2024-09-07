f22caf4d7a509333471ae668c149ae41484c521c
Ford Heacock <ford.heacock@dapperlabs.com>
2024-09-07 12:33:08 -0400

    update template

--- src/pages/api/v1/notes/delete.ts
+++ src/pages/api/v1/notes/delete.ts
@@ -5,8 +5,9 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
 ) {
-	if (!req.body.noteId)
+	if (!req.body.noteId) {
 		return res.status(400).json({ error: 'Missing noteId', note: null });
+	}
 
 	await prisma.note.delete({
 		where: {
